40fde44baba22a78f3e4cd0b9272f9ecd574a841
nishankumartripathy <tripathynishankodala@gmail.com>
2020-08-22 12:00:29 +0530

    abc

---
+++ nishalogin.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 22, 2020 at 08:26 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nishalogin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nishalogin`
+--
+
+CREATE TABLE `nishalogin` (
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `nishalogin`
+--
+
+INSERT INTO `nishalogin` (`name`, `password`) VALUES
+('12345', ''),
+('nishan', ''),
+('jdfgdj', '122344'),
+('avinash', '12345'),
+('avinasha', '12345'),
+('keshab', '5678'),
+('hari', '12345'),
+('mondal', '123');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
